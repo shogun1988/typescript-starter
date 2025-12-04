10ba8943d155ec109767a469880bae5efac48f5b
shogun1988 <nokahvandenhoven@gmail.com>
2025-12-04 14:30:56 +0100

    first commit

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('profiles')
-export class ProfilesController {}
+export class ProfilesController {
+
+    @Get()
+    findAll() {
+        return [
+            {
+                "id": 1,
+                "name": "nokah",
+                "age": 65,
+                "address": {
+                    "straat": "stationstraat",
+                    "nummer": 789,
+                    "postcode": 2600
+                }
+            }
+        ];
+    }   
+}
